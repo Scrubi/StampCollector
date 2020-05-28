8a989aeb6a3089ee8f1afad2fff0aa86877d8394
Scrubi <51309968+Scrubi@users.noreply.github.com>
2020-05-28 12:16:26 +0300

    Updates

---
+++ precache-manifest.ccd3a8d0617fa696c17e6adfd2977e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff44b1eae4dde114afe6ec8d5a5e1baf",
+    "url": "/StampCollector/index.html"
+  },
+  {
+    "revision": "3a709848e756df0b8766",
+    "url": "/StampCollector/static/css/main.733ebcf0.chunk.css"
+  },
+  {
+    "revision": "2566c90246ee7bed2964",
+    "url": "/StampCollector/static/js/2.4063caf4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StampCollector/static/js/2.4063caf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a709848e756df0b8766",
+    "url": "/StampCollector/static/js/main.d2722c0f.chunk.js"
+  },
+  {
+    "revision": "85100699169bf7f76f73",
+    "url": "/StampCollector/static/js/runtime-main.bd513916.js"
+  }
+]);
